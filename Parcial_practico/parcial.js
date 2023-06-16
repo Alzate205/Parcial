c959396c5d87ad01f4c1e8013247a94c259cb755
Alzate205 <132091640+Alzate205@users.noreply.github.com>
2023-06-16 11:06:48 -0500

    Add files via upload

---
+++ Parcial_practico/parcial.js
@@ -0,0 +1,32 @@
+let estudiantes = [
+    {
+        nombre: "Samuel",
+        edad: 17,
+        grado: "Primero",
+        promedio: 3.8,
+        carrera: "Ingeniería"
+    },
+    {
+        nombre: "Arle",
+        edad: 27,
+        grado: "Segundo",
+        promedio: 4.0,
+        carrera: "Software"
+    },
+    {
+        nombre: "Carolina",
+        edad: 20,
+        grado: "Tercero",
+        promedio: 4.5,
+        carrera: "Derecho"
+    },
+    {
+        nombre: "Catalina",
+        edad: 21,
+        grado: "Cuarto",
+        promedio: 3.0,
+        carrera: "Arquitectura"
+    }
+];
+
+console.log(estudiantes);
